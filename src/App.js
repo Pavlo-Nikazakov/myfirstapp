3d3487ce1a125412363fa39c14b6f7a88e191c16
Pavlo-Nikazakov <pavlo.nikazakov@gmail.com>
2020-06-29 14:59:24 +0200

    fixed the import path for Pokemon

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Title from './components/Title'
-import Pokemon from '../../../untitled:/Users/gebruiker/Library/Mobile Documents/com~apple~CloudDocs/codaisseur/code/week-2/myfirstapp/src/components/^Pokemon';
-
+import Pokemon from './components/Pokemon'
 function App() {
   return (
     <div className="App">
