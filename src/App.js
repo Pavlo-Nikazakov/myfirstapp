ec42acaa4c100d68aedad4bc84b878bcdd1e782b
Pavlo-Nikazakov <pavlo.nikazakov@gmail.com>
2020-06-29 13:17:47 +0200

    fixed the spelling

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
             target="_blank" 
             rel="noopener noreferrer"
           >
-            Good Mornin!
+            Good Morning!
           </a>
         </header>
     </div>
